76cc414441dc10e9a56c51ccc7dc5a66ff9f856b
IFAKeV <edv@ifak-bochum.de>
2024-09-19 15:58:36 +0200

    Create param.ps1

---
+++ ChatGPT-SecondVersion/param.ps1
@@ -0,0 +1,10 @@
+param(
+    [string]$DriveLetter = "C",
+    [int]$ThresholdGB = 10,
+    [string]$SmtpServer = "smtp",
+    [string]$SmtpFrom = "@",
+    [string]$SmtpTo = "@",
+    [string]$SmtpUser = "@",
+    [string]$PasswordFile = "C:\Path\To\EncryptedPassword.txt",
+    [string]$ErrorLogPath = "C:\Path\To\ErrorLog.txt"
+)
